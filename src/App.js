76b67008b5562d24ed7022461a339124307ebe40
kisar18 <martinpur99@seznam.cz>
2022-11-09 08:57:48 +0100

    new homepage

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Profile from './components/Profile';
+import HomePage from './components/HomePage';
 
 function App() {
 
   return (
     <BrowserRouter className="app">
       <Routes>
-        <Route path='/' exact element={<RegistrationForm />} />
+        <Route path='/' exact element={<HomePage />} />
         <Route path='/login' exact element={<LoginForm />} />
+        <Route path='/register' exact element={<RegistrationForm />} />
         <Route path='/books' exact element={<Books />} />
         <Route path='/profile' exact element={<Profile />} />
       </Routes>
---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+function HomePage() {
+  return <Box>HomePage</Box>;
+}
+
+export default HomePage;
